c219b9f777f8f8e838b595f766984350343c00f1
einan <ebubekirinan94@gmail.com>
2023-11-07 17:49:06 +0300

    auth pages small fixes

--- app/(auth)/forgotpassword/page.tsx
+++ app/(auth)/forgotpassword/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function ForgotPassword() {
     return (
-        <main className="flex flex-col items-center justify-center h-screen bg-slate-100">
+        <main className="flex flex-col items-center justify-center h-screen">
             <div className="flex flex-col p-10 md:w-1/3 w-full border-2 rounded-lg border-cyan-800/10 shadow-lg shadow-slate-600/25 bg-white">
                 <h2 className="text-center text-3xl text-slate-500 mb-5 pb-2 border-b-2 border-b-slate-100">Reset Password</h2>
                 <form className="flex flex-col">
---
+++ app/(home)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main className="flex h-screen justify-center border">
+            <p className="w-full md:w-1/2 bg-white">Welcome!</p>
+        </main>
+    )
+}
